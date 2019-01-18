65688e721d11bc99f17914e21144d423b3b694ca
shafana <shafana@wso2.com>
2019-01-18 14:35:40 +0530

    Update script to execute all dbScripts in folder

--- scripts/packer/packer-resources/util/bashScripts/provision_db_scripts.sh
+++ scripts/packer/packer-resources/util/bashScripts/provision_db_scripts.sh
@@ -16,9 +16,9 @@
 
 #!/bin/bash
 echo "Running DB scripts..."
-for dbscript in *.sql
+for dbscript in /home/ubuntu/$1/dbScripts/*.sql
   do
    echo "Executing script: $dbscript ..."
-   mysql -u CF_DB_USERNAME -pCF_DB_PASSWORD -h CF_DB_HOST -P CF_DB_PORT < /home/ubuntu/$1/dbScripts/dbscript.sql
+   mysql -u CF_DB_USERNAME -pCF_DB_PASSWORD -h CF_DB_HOST -P CF_DB_PORT < $dbscript
  done
 

8b09ea79ddb9e0f2b2abc8621b1ee2f4095f2acb
ShafanaSafwan <kreepyprincess07@outlook.com>
2019-08-19 19:24:36 +0530

    Fix typo

--- scripts/packer/packer-resources/util/bashScripts/wso2is/5.7.0/is/MySQL/provision_db_scripts.sh
+++ scripts/packer/packer-resources/util/bashScripts/wso2is/5.7.0/is/MySQL/provision_db_scripts.sh
@@ -34,6 +34,6 @@ for regdbscript in /home/wso2user/dbScripts/MySQL/registry/*.sql
 for identitydbscript in /home/wso2user/dbScripts/MySQL/identity/*.sql
   do
    echo "Executing script: $regdbscript ..."
-   mysql -u CF_DB_USERNAME -pCF_DB_PASSWORD -h CF_DB_HOST -P CF_DB_PORT < identitydbscript
+   mysql -u CF_DB_USERNAME -pCF_DB_PASSWORD -h CF_DB_HOST -P CF_DB_PORT < $identitydbscript
  done
 
